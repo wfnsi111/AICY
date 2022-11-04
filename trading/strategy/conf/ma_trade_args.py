f76f8cd834ebbac01a60d9da51e62debf6f18d4f
LK <771801923@qq>
2022-11-04 12:15:21 +0800

    11

---
+++ trading/strategy/conf/ma_trade_args.py
@@ -0,0 +1,67 @@
+args_dict = {
+    '5m': {
+        "ma_percent": 0.003,
+        'bar1': '1m',
+        'stop_loss': 1,
+        'set_profit': 5,
+        'risk_control': 0.007,
+    },
+    '15m': {
+        "ma_percent": 0.003,
+        'bar1': '1m',
+        'stop_loss': 1,
+        'set_profit': 5,
+        'risk_control': 0.01,
+    },
+    '30m': {
+        "ma_percent": 0.005,
+        'bar1': '3m',
+        'stop_loss': 1,
+        'set_profit': 5,
+        'risk_control': 0.02,
+    },
+    '1H': {
+        "ma_percent": 0.007,
+        'bar1': '3m',
+        'stop_loss': 2,
+        'set_profit': 5,
+        'risk_control': 0.018,
+    },
+    '2H': {
+        "ma_percent": 0.01,
+        'bar1': '5m',
+        'stop_loss': 2,
+        'set_profit': 5,
+        'risk_control': 0.027,
+    },
+    '4H': {
+        "ma_percent": 0.009,
+        'bar1': '5m',
+        'stop_loss': 2,
+        'set_profit': 5,
+        'risk_control': 0.03,
+    },
+    '6H': {
+        "ma_percent": 0.01,
+        'bar1': '5m',
+        'stop_loss': 3,
+        'set_profit': 5,
+        'risk_control': 0.037,
+    },
+
+    '12H': {
+        "ma_percent": 0.01,
+        'bar1': '15m',
+        'stop_loss': 3,
+        'set_profit': 10,
+        'risk_control': 0.045,
+    },
+    '1D': {
+        "ma_percent": 0.01,
+        'bar1': '15m',
+        'stop_loss': 3,
+        'set_profit': 10,
+        'risk_control': 0.075,
+    },
+
+}
