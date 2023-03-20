b9b3416c152a272ad063d58e3b928160a4d1497a
LK <771801923@qq>
2023-03-20 12:04:54 +0800

    1

--- trading/admin.py
+++ trading/admin.py
@@ -3,18 +3,60 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Trader, AccountInfo, Strategy, OrderInfo, NewUser, AccountAssetBills
-
+from .models import *
 
 admin.site.site_header = '交易员管理'
 admin.site.site_title = '交易员管理'
 admin.site.index_title = '交易员管理'
 
 
-admin.site.register(Trader)
-admin.site.register(AccountInfo)
-admin.site.register(Strategy)
-admin.site.register(OrderInfo)
-admin.site.register(NewUser)
-admin.site.register(AccountAssetBills)
+# admin.site.register(Trader)
+# admin.site.register(AccountInfo)
+# admin.site.register(Strategy)
+# admin.site.register(OrderInfo)
+# admin.site.register(NewUser)
+# admin.site.register(AccountAssetBills)
 
+
+@admin.register(Trader)
+class TraderAdmin(admin.ModelAdmin):
+    list_display = ("name", "strategy", "lever", "trader_text")  # 需要显示的字段
+    search_fields = list_display
+    list_filter = ("name", "strategy", "lever")
+
+
+@admin.register(AccountInfo)
+class AccountInfoAdmin(admin.ModelAdmin):
+    list_display = ("account_text", "trader", "api_key", "balance", "promoter", "is_active")  # 需要显示的字段
+    search_fields = list_display
+    list_filter = list_display
+
+
+@admin.register(Strategy)
+class StrategyAdmin(admin.ModelAdmin):
+    list_display = ("name", "status", "ma", "bar2", "instid", "accountinfo", "msg")  # 需要显示的字段
+    search_fields = list_display
+    list_filter = ("accountinfo", "name",)
+
+
+@admin.register(OrderInfo)
+class OrderInfoAdmin(admin.ModelAdmin):
+    list_display = ("accountinfo", "strategyid", "posside", "side", "sz", "avgpx", "closeavgpx", "pnl", "msg",
+                    "create_time", "update_time")
+    search_fields = list_display
+    list_filter = ("accountinfo",)
+
+
+@admin.register(PlaceAlgo)
+class PlaceAlgoAdmin(admin.ModelAdmin):
+    list_display = ("accountinfo", "instid", "posside", "side", "sz", "tptriggerpx", "tpordpx", "sltriggerpx",
+                    "slordpx", "smsg")  # 需要显示的字段
+    search_fields = list_display
+    list_filter = ("accountinfo",)
+
+
+@admin.register(NewUser)
+class NewUserAdmin(admin.ModelAdmin):
+    list_display = ("name", "create_time", "update_time")
+    search_fields = list_display
+    list_filter = list_display
--- trading/models.py
+++ trading/models.py
@@ -41,9 +41,9 @@ class Trader(models.Model):
     name = models.CharField(verbose_name="昵称", max_length=50)
     account = models.CharField(verbose_name="登陆账户", max_length=50)
     password = models.CharField(verbose_name="密码", max_length=50)
-    strategy = models.CharField(max_length=50, default='MaTrade', editable=False)
-    trading = models.BooleanField(default=False, editable=False)
-    trader_text = models.CharField(max_length=200, null=True, editable=False)
+    strategy = models.CharField(verbose_name="策略", max_length=50, default='MaTrade')
+    trading = models.BooleanField(default=False)
+    trader_text = models.CharField(verbose_name="备注", max_length=200, null=True, blank=True)
     lever = models.IntegerField(verbose_name="交易等级", default=1, null=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
@@ -54,8 +54,8 @@ class Trader(models.Model):
 
 class AccountInfo(models.Model):
     class Meta:
-        verbose_name = "交易账户信息"
-        verbose_name_plural = "交易账户信息"
+        verbose_name = "交易账户表"
+        verbose_name_plural = "交易账户表"
 
     data = (
         ('0', '实盘'),
@@ -68,7 +68,7 @@ class AccountInfo(models.Model):
         (2, '取消'),
     )
 
-    trader = models.ForeignKey(Trader, on_delete=models.CASCADE)
+    trader = models.ForeignKey(Trader, on_delete=models.CASCADE, verbose_name='所属交易员')
     account_text = models.CharField(verbose_name='用户名称', max_length=50)
     api_key = models.CharField(max_length=50, unique=True)
     secret_key = models.CharField(max_length=50)
@@ -76,15 +76,15 @@ class AccountInfo(models.Model):
     balance = models.FloatField(verbose_name='保证金（USD）', default=-1)
     init_balance = models.FloatField(verbose_name='保证金（USD）', default=-1)
     flag = models.CharField(default='0', max_length=10, choices=data, editable=False)
-    email = models.CharField(max_length=50, null=True)
-    phone = models.CharField(max_length=50, null=True)
-    promoter = models.CharField(verbose_name='推介人姓名', max_length=50, null=True)
+    email = models.CharField(max_length=50, null=True, blank=True)
+    phone = models.CharField(max_length=50, null=True, blank=True)
+    promoter = models.CharField(verbose_name='推介人姓名', max_length=50, null=True, blank=True)
     status = models.IntegerField(default=0, editable=False)
     operate = models.IntegerField(default=1, editable=False)
     msg = models.TextField(null=True, blank=True, editable=False)
-    strategy_name = models.CharField(max_length=20, null=True, editable=False)
+    strategy_name = models.CharField(max_length=20, null=True, blank=True, editable=False)
     bar2 = models.CharField(max_length=10, null=True, editable=False)
-    is_active = models.IntegerField(verbose_name='激活账户', default=1, choices=active_choices)
+    is_active = models.IntegerField(verbose_name='激活状态', default=1, choices=active_choices)
 
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
@@ -100,13 +100,13 @@ class Strategy(models.Model):
 
     name = models.CharField(verbose_name='策略名称', max_length=50)
     is_active = models.IntegerField(default=0, editable=False)
-    status = models.IntegerField(default=0, editable=False)
+    status = models.IntegerField(verbose_name='状态', default=0, editable=False)
     ma = models.CharField(verbose_name='均线', null=True, max_length=10)
     bar2 = models.CharField(verbose_name='大周期', null=True, max_length=10)
     instid = models.CharField(verbose_name='合约', null=True, max_length=50)
     accountinfo = models.TextField(verbose_name='账户', null=True, blank=True, editable=False)
     signalinfo = models.TextField(verbose_name='信号信息', null=True, blank=True, editable=False)
-    msg = models.TextField(null=True, blank=True, editable=False)
+    msg = models.TextField(verbose_name='错误日志', null=True, blank=True, editable=False)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
 
@@ -119,14 +119,14 @@ class OrderInfo(models.Model):
         verbose_name = "订单信息"
         verbose_name_plural = "订单信息"
 
-    accountinfo = models.ForeignKey(AccountInfo, on_delete=models.CASCADE)
-    strategyid = models.IntegerField(default=0, null=True)
-    ordid = models.CharField(max_length=50)
-    instid = models.CharField(max_length=50)
+    accountinfo = models.ForeignKey(AccountInfo, on_delete=models.CASCADE, verbose_name='账户')
+    strategyid = models.IntegerField(verbose_name='策略ID', default=0, null=True)
+    ordid = models.CharField(verbose_name='订单ID', max_length=50)
+    instid = models.CharField(verbose_name='合约', max_length=50)
     posside = models.CharField(max_length=10, null=True)
     side = models.CharField(max_length=10, null=True)
-    avgpx = models.CharField(max_length=20, null=True)
-    closeavgpx = models.CharField(max_length=20, null=True)
+    avgpx = models.CharField(verbose_name='开仓均价', max_length=20, null=True)
+    closeavgpx = models.CharField(verbose_name='平仓均价', max_length=20, null=True)
     tptriggerpx = models.CharField(max_length=20, null=True)
     tpordpx = models.CharField(max_length=20, null=True)
     sltriggerpx = models.CharField(max_length=20, null=True)
@@ -138,18 +138,18 @@ class OrderInfo(models.Model):
     tdmode = models.CharField(default='cross', max_length=20, null=True)
     ccy = models.CharField(default='USDT', max_length=20, null=True)
     ordtype = models.CharField(default='market', max_length=20, null=True)
-    sz = models.CharField(default='0', max_length=20, null=True)
+    sz = models.CharField(verbose_name='仓位', default='0', max_length=20, null=True)
     px = models.CharField(max_length=20, null=True)
     accfillsz = models.CharField(max_length=20, null=True)
-    pnl = models.CharField(max_length=20, null=True)
+    pnl = models.CharField(verbose_name='收益', max_length=20, null=True)
     lever = models.CharField(max_length=10, null=True)
     fee = models.CharField(max_length=10, null=True)     # 手续费
 
     close_position = models.IntegerField(default=0)
     status = models.IntegerField(default=0)
     msg = models.TextField(null=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(verbose_name='开始时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='结束时间', auto_now=True)
     # date = models.DateTimeField('date published')
 
     order_ctime = models.CharField(max_length=40, null=True)    # 订单创建时间
@@ -167,8 +167,8 @@ class NewUser(models.Model):
 
     name = models.CharField(verbose_name='登录名', max_length=20)
     password = models.CharField(verbose_name='密码', max_length=20)
-    create_time = models.DateTimeField(auto_now_add=True)
-    update_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(verbose_name='注册时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='上次修改时间', auto_now=True)
 
     def __str__(self):
         return self.name
@@ -179,7 +179,7 @@ class AccountAssetBills(models.Model):
         verbose_name = "资金流水"
         verbose_name_plural = "资金流水"
 
-    accountinfo = models.ForeignKey(AccountInfo, on_delete=models.CASCADE)
+    accountinfo = models.ForeignKey(AccountInfo, verbose_name='账户', on_delete=models.CASCADE)
     billid = models.CharField(verbose_name='账单 ID', max_length=20, unique=True)
     bal = models.CharField(verbose_name='账户层面的余额数量', max_length=20)
     balchg = models.CharField(verbose_name='账户层面的余额变动数量', max_length=20)
@@ -198,7 +198,7 @@ class PlaceAlgo(models.Model):
         verbose_name = "策略委托订单"
         verbose_name_plural = "策略委托订单"
 
-    accountinfo = models.ForeignKey(AccountInfo, on_delete=models.CASCADE)
+    accountinfo = models.ForeignKey(AccountInfo, on_delete=models.CASCADE, verbose_name='账户')
     algoid = models.CharField(verbose_name='委托订单 ID', max_length=50, unique=True)
     clordid = models.CharField(verbose_name='自定义 ID', max_length=50, null=True)
     orderid = models.CharField(verbose_name='开仓 ID', max_length=50)
